4bc87d57dd153ba89043487ff9bafbbe28951dd9
Olagbenro Samuel <96908482+samueleniola@users.noreply.github.com>
2026-05-17 19:54:38 +0100

    Update Credit Card Fraud Detection App.py

--- Credit Card Fraud Detection App.py
+++ Credit Card Fraud Detection App.py
@@ -1,11 +1,11 @@
 import streamlit as st
 import numpy as np
-import joblib
+import joblib as jb
 
 # Load model and scaler
-model = joblib.load("fraud_detection_model.joblib")
-scaler = joblib.load("scaler.joblib")
-features = joblib.load("feature_names.joblib")
+model = jb.load("fraud_detection_model.joblib")
+scaler = jb.load("scaler.joblib")
+features = jb.load("feature_names.joblib")
 
 st.title("Credit Card Fraud Detection App")
 
